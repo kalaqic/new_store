1429d074a275d2eb8d5cb4796664d9b1cc4222ee
kalaqic <davidkalabic777@gmail.com>
2023-05-20 17:06:40 +0200

    refactor search screen, add firebase banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:store_new/views/buyers/main_scr.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async{
-
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
 
   runApp ( MaterialApp(
+
     //scroll on web
     scrollBehavior: AppScrollBehavior(),
     debugShowCheckedModeBanner: false,
--- lib/views/buyers/components/search/banner.dart
+++ lib/views/buyers/components/search/banner.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class CustomBannerWidget extends StatefulWidget {
@@ -8,11 +9,25 @@ class CustomBannerWidget extends StatefulWidget {
 }
 
 class _CustomBannerWidgetState extends State<CustomBannerWidget> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
+  final List _bannerImage = [];
 
+  getBanners(){
+    return _firestore
+    .collection('banners')
+    .get()
+    .then((QuerySnapshot querySnapshot){
+      for (var doc in querySnapshot.docs) {
+        setState(() {
+          _bannerImage.add(doc['image']);
+        });
+      }
+    });
+  }
   @override
   void initState() {
-
+    getBanners();
     super.initState();
   }
 
@@ -28,13 +43,14 @@ class _CustomBannerWidgetState extends State<CustomBannerWidget> {
           color: Colors.yellow.shade900,
           borderRadius: BorderRadius.circular(10),
         ),
-        child: PageView(
-          children: const [
-            Center(child: Text('Banner 1')),
-            Center(child: Text('Banner 2')),
-            Center(child: Text('Banner 3')),
-          ],
-        )
+        child: PageView.builder(
+          itemCount: _bannerImage.length,
+          itemBuilder:(context, index) {
+          return Image.network(
+            _bannerImage[index],
+            fit: BoxFit.cover,
+            );
+        },),
       ),
     );
   }
